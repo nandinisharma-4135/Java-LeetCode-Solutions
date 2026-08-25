84e40ec1c95b096123c2dc8a506b08d2830c6580
Nandini Sharma <nandinisharma4135@gmail.com>
2026-08-25 20:41:18 +0530

    Time: 0 ms (100.00%) | Memory: 48.6 MB (14.11%) - LeetSync

---
+++ 792-binary-search/binary-search.java
@@ -0,0 +1,27 @@
+class Solution {
+    public int search(int[] nums, int target) {
+       int ans = -1;
+       int left = 0;
+       int right = nums.length - 1;
+       
+       while(left<=right)
+      {int mid = (left + right)/2;
+       
+        if(nums[mid]==target)
+        {
+            ans = mid;
+            break;
+        }
+        else if(nums[mid]<target)
+        {
+            left = mid + 1;
+        }
+        else{
+            right = mid - 1;
+        }
+       }
+      return ans;
+
+
+    }
+}
