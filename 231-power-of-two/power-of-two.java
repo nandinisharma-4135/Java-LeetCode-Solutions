b66d9524308b3615281961aaf08a1adc858f095d
Nandini Sharma <nandinisharma4135@gmail.com>
2026-09-01 12:19:05 +0530

    Time: 1 ms (96.59%) | Memory: 42.7 MB (36.63%) - LeetSync

---
+++ 231-power-of-two/power-of-two.java
@@ -0,0 +1,22 @@
+class Solution {
+    public boolean isPowerOfTwo(int n) {
+        
+        if(n == 0 | n<0)
+        {
+            return false;
+        }
+            int a = n & (n-1);
+            if(a == 0)
+            {
+                return true;
+            }
+            else
+            {
+                return false;
+            }
+        
+        
+
+        
+    }
+}
